7f2c7d5608ae600983f9c41359228ee3ef2cb8ea
siyam79 <siyamahmed3827@gmail.com>
2024-01-13 12:27:40 +0600

    category wise all data show and emplements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 
 import { useEffect, useState } from 'react'
 import './App.css'
-
+import { FaIdBadge } from "react-icons/fa6";
+import { BiMessageRounded } from "react-icons/bi";
+import { FaBuffer } from "react-icons/fa6";
+import { FaHotdog } from "react-icons/fa6";
+import { BsCalendarDate } from "react-icons/bs";
 function App() {
 
 
@@ -34,7 +38,7 @@ function App() {
 
   const [cards, setCards] = useState([])
   useEffect(() => {
-    fetch('data.json')
+    fetch('http://localhost:5000/v1/allTask')
       .then(res => res.json())
       .then(data => setCards(data))
   }, [])
@@ -60,13 +64,64 @@ function App() {
                     `}> {category.name} </h2>
                   <button className='btn'>0</button>
                 </div>
-                <div className=' h-[100vh]  w-[380px] p-4 border-2 border-[#924AEF] rounded-b-xl  overflow-y-auto scrollbar-to-do space-y-4'>
+                <div className=' h-[100vh]  w-[380px]  border-2 border-[#924AEF] rounded-b-xl  overflow-y-auto scrollbar-to-do space-y-4'>
 
                   {
-                    cards.map((card, i) => <div key={i} className=' bg-white py-24 '>
 
-                    </div>)
+                    cards.map((data, i) => data.status === category.name ? <div key={i} className=' bg-white h-[200px] ' >
+                      <div className=' flex items-center justify-between px-2 pt-2 '>
+                        <div className=" flex items-center just gap-1 ">
+                          <img alt="" className='w-12  h-10 rounded-full' src="https://i.ibb.co/vsFt72P/images-3.jpg" />
+                          <h1>Client Name </h1>
+                        </div>
+                        <div>
+                          <div className=" flex items-center just gap-1 ">
+                            <img alt="" className='w-[36px]  h-10 rounded-full' src="https://i.ibb.co/xCdnLKR/images-4.jpg" />
+                            <h1> {data.name} </h1>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className=' flex items-center justify-between px-2 mt-6'>
+                        <div className='  flex items-center gap-2'>
+                          <FaBuffer></FaBuffer>
+                          <h1> {data.description}....... </h1>
+                        </div>
+
+                        <button className=' flex items-center bg-base-300 px-2 rounded-sm '>
+                          <FaIdBadge></FaIdBadge>
+                          1/2
+                        </button>
+
+                      </div>
+                      <div className=' flex items-center mt-10 px-2 '>
+                        <div className='flex items-center justify-around gap-3'>
+                          <img alt="" className='w-[36px]  h-10 rounded-full' src="https://i.ibb.co/xCdnLKR/images-4.jpg" />
+                          <img alt="" className='w-12  h-10 rounded-full' src="https://i.ibb.co/vsFt72P/images-3.jpg" />
+                          <h1 className='bg-base-300 rounded-full px-1'> 12+ </h1>
+                          <div>
+                          <h1 className='flex items-center gap-1'>
+                          <BiMessageRounded></BiMessageRounded> 
+                          20
+                          </h1>
+                          </div>
+                         
+                          <h1 className='flex items-center gap-1'>
+                          <FaHotdog></FaHotdog> 
+                          25
+                          </h1>
+
+                          <h1 className='flex items-center gap-1'>
+                            <BsCalendarDate></BsCalendarDate>
+                            {data.date}
+                          </h1>
+
+                        </div>
+                      </div>
+                    </div> : <></>)
+
                   }
+
                 </div>
               </div>
             </div>)
